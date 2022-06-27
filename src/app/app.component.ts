94cc7e3fb295efeab42dad6e928411996a2b818e
Leonel López <31909505+LeoLopez02@users.noreply.github.com>
2022-06-26 20:08:38 -0400

    Added new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'task-tracker-v2';
-}
+export class AppComponent {}
